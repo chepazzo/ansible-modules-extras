c5fa2538686b3d310ceb2f1085aefc8bc8830d72
Biancaniello, Michael <m.biancaniello@teamaol.com>
2015-12-10 11:18:38 -0500

    Changed version_added to 2.0.

--- network/trigger/trigger_command.py
+++ network/trigger/trigger_command.py
@@ -29,7 +29,7 @@ description:
   - Trigger is a toolkit used to connect to network devices. While Trigger can utilize a device's API, its main use with Ansible would be to interface with devices for which the only connection methods are telnet or ssh.
   - You can install trigger through pip or follow on github (http://github.com/trigger).
   - Read the docs: http://triggerproject.org/en/latest/
-version_added: "1.9"
+version_added: "2.0"
 category: network
 author: Mike Biancaniello (@chepazzo)
 requirements:
